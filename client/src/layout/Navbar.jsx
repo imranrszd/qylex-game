105714a862e6aba17163a8310a06c15776212ad3
imran <imranrszd@gmail.com>
2026-02-12 17:46:05 +0800

    another minor tweak, logo to home page

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -11,7 +11,9 @@ export default function Navbar({ onViewChange, currentView }) {
     <nav className="fixed top-0 w-full z-50 border-b border-slate-700/50 backdrop-blur-md bg-[#0B1D3A]/90">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
-          <div className="flex items-center gap-2 cursor-pointer group" onClick={() => onViewChange('home')}>
+          <Link className="flex items-center gap-2 cursor-pointer group"
+            to="/"
+            onClick={() => setIsOpen(false)}>
             <div className="w-20 h-10 bg-linear-to-tr pt-1 from-blue-600 to-cyan-400 rounded-xl flex items-center justify-center shadow-lg shadow-blue-500/20 group-hover:shadow-blue-500/40 transition-all">
               {/* <Zap className="text-white w-6 h-6 fill-current" /> */}
               <img src={qylexLogo} alt="Qylex logo" />
@@ -21,7 +23,7 @@ export default function Navbar({ onViewChange, currentView }) {
               <span className="text-2xl font-bold text-white tracking-tight">Qylex<span className="text-cyan-400">Game</span></span>
               <span className="text-[10px] text-slate-400 uppercase tracking-widest font-semibold">Premium TopUp</span>
             </div>
-          </div>
+          </Link>
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-8">
               <NavLink
